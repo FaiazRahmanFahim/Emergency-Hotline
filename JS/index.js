3cccda3c24af13bab4cb025e1b4a3491068f48c1
FaiazRahmanFahim <faiazrahman12.std@gmail.com>
2025-08-28 14:29:54 +0600

    add copy feature

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 //console.log("JavaScript Page Connected Successfully");
 
-// -------- Heart Count -------- //
+// -------- Heart Count Feature -------- //
 const countHearts = document.getElementsByClassName("input-heart");
 //console.log(countHearts);
 for (const heart of countHearts) {
@@ -11,5 +11,33 @@ for (const heart of countHearts) {
     const sum = totalHeart + 1;
     document.getElementById("total-heartCount").innerText = sum;
     //console.log(sum);
+  });
+}
+
+// -------- Copy Button Feature -------- //
+const copyButtons = document.querySelectorAll(".btn-copy");
+//console.log(copyButton);
+for (const button of copyButtons) {
+  button.addEventListener("click", function () {
+    const card = button.closest("div[id]");
+    //console.log(card);
+    const copyText = card.querySelector(".copy-num").innerText;
+    //console.log(copyText);
+
+    const totalCopyCount = parseInt(
+      document.getElementById("total-copyCount").innerText
+    );
+
+    const sum = totalCopyCount + 1;
+    document.getElementById("total-copyCount").innerText = sum;
+
+    navigator.clipboard
+      .writeText(copyText)
+      .then(() => {
+        alert(`The Copied Number is: ${copyText}`);
+      })
+      .catch(() => {
+        alert("Failed to copy!");
+      });
   });
 }
